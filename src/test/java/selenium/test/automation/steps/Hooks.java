6814723d42ebc31e63227ac1d05ca3b2a4f6e926
Estefanía Turín <tur.senn@gmail.com>
2024-02-02 00:22:28 -0300

    hooks

--- src/test/java/selenium/test/automation/steps/Hooks.java
+++ src/test/java/selenium/test/automation/steps/Hooks.java
@@ -1,2 +1,27 @@
-package selenium.test.automation.steps;public class Hooks {
+package selenium.test.automation.steps;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import selenium.test.automation.pages.BasePage;
+
+
+public class Hooks extends BasePage {
+
+    public Hooks() {
+        super(driver);
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            scenario.log("Scenario failing, please refer to the image attached to this report");
+            final byte[] screenshot = ((TakesScreenshot) driver)
+                    .getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "Screenshot of the error");
+        }
+    }
+
 }
